fdca88bbeb765c3c4346fce3ccd78c0189c1b63e
YagizYigit <71469815+YagizYigit@users.noreply.github.com>
2022-01-28 20:33:53 +0300

    Update KriptoApi.java

--- src/main/java/com/serdaryigit/kline/KriptoApi.java
+++ src/main/java/com/serdaryigit/kline/KriptoApi.java
@@ -8,9 +8,9 @@ import retrofit2.http.GET;
 public interface KriptoApi {
     String sembol = "FTMUSDT";
 
-  @GET("/api/v3/klines?symbol=" + sembol + "&interval=1m")
- //  @GET("/api/v3/depth?symbol=" + sembol )
-   //  @GET("/api/v3/aggTrades?symbol=" + sembol +"&limit=5")
+        @GET("/api/v3/klines?symbol=" + sembol + "&interval=1m")         //   kline kısmı gelen json yapısından dolayı okunmuyor
+
+    //  @GET("/api/v3/aggTrades?symbol=" + sembol +"&limit=5")           //   aggTrades kısmı data geliyor
 
   Call<KlineModel> getData();
 
