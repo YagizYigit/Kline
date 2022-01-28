667e7ddbac1b5570f4b52b56c1bb97d7ed60122f
YagizYigit <71469815+YagizYigit@users.noreply.github.com>
2022-01-28 20:20:09 +0300

    Update MainActivity.java

--- src/main/java/com/serdaryigit/kline/MainActivity.java
+++ src/main/java/com/serdaryigit/kline/MainActivity.java
@@ -53,15 +53,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       // jasonParsee();
+       
 
 
-        Gson gson = new Gson();
-
-
-    //    Type newKline = new TypeToken<ArrayList<KlineModel>>(){}.getType();
-      //  ArrayList<KlineModel> newKlineModel = gson.fromJson(jsonCon, newKline );
-    //    System.out.println(newKlineModel);
+        Gson gson = new Gson().newBuilder().create();
 
         Dispatcher dispatcher = new Dispatcher();
         dispatcher.setMaxRequestsPerHost(500);
@@ -74,12 +69,12 @@ public class MainActivity extends AppCompatActivity {
 
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(sharedClient)
                 .build();
 
-       loadData();
-        //  jasonParsee();
+       loadData();    
+       
 
 
     }
@@ -104,113 +99,50 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+/* -------------------------------BU KISIM URL'DEN GELEN  DATANIN JSON HALİ  (DATA POSTMAN ÜZERİNDEN ALINMIŞTIR...)------------------------
+[
+    [
+        1643389920000,
+        "37140.00000000",
+        "37141.18000000",
+        "37052.17000000",
+        "37054.91000000",
+        "22.08144000",
+        1643389979999,
+        "819179.74828210",
+        705,
+        "9.79463000",
+        "363212.47614610",
+        "0"
+    ],
+    [
+        1643389980000,
+        "37054.90000000",
+        "37076.85000000",
+        "37029.75000000",
+        "37029.75000000",
+        "12.58066000",
+        1643390039999,
+        "466166.74746430",
+        425,
+        "4.74596000",
+        "175883.77053670",
+        "0"
+    ]
+]
+
+-----------------------------------------------------------END-------------------------
+*/
+    
+    
 
 
-    public void jasonParsee () {
-        String jsonString = "[[1642850280000,\"35291.18000000\",\"35318.00000000\",\"35250.15000000\",\"35304.72000000\",\"114.16537000\",1642850339999,\"4027913.53985850\",1368,\"61.48630000\",\"2168980.00679440\",\"0\"],[1642850340000,\"35304.73000000\",\"35305.26000000\",\"35265.89000000\",\"35280.60000000\",\"34.95279000\",1642850399999,\"1233376.84292890\",980,\"15.87426000\",\"560150.87425420\",\"0\"]]";
-        try {
-            JSONArray jsonArray = new JSONArray(jsonString);
-            JSONArray jsonArray2 = jsonArray.getJSONArray(0);
 
-            for (int i = 0; i < jsonArray2.length(); i++) {
-                String valueString = jsonArray2.getString(i);
-                System.out.println("\"" + array[i] + "\"" + ": " +"\"" + valueString+ "\"");
-                // Log.e("json", (" " + array[i] + " " + " : " + valueString));
-                new ToStringBuilder(this, BinanceApiConstants.TO_STRING_BUILDER_STYLE)
-                        .append(array[i], i).toString();
-
-            }
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-
-        return ;
-    }
-
-/*
-    public Converter.Factory jasonParsee() {
-        String jsonString = "[[1642850280000,\"35291.18000000\",\"35318.00000000\",\"35250.15000000\",\"35304.72000000\",\"114.16537000\",1642850339999,\"4027913.53985850\",1368,\"61.48630000\",\"2168980.00679440\",\"0\"],[1642850340000,\"35304.73000000\",\"35305.26000000\",\"35265.89000000\",\"35280.60000000\",\"34.95279000\",1642850399999,\"1233376.84292890\",980,\"15.87426000\",\"560150.87425420\",\"0\"]]";
-        try {
-            JSONArray jsonArray = new JSONArray(jsonString);
-            JSONArray jsonArray2 = jsonArray.getJSONArray(0);
-
-            for (int i = 0; i < jsonArray2.length(); i++) {
-                String valueString = jsonArray2.getString(i);
-                Log.e("json", ("\"" + array[i] + "\"" + ": " + "\"" + valueString + "\""));
-                new ToStringBuilder(this, BinanceApiConstants.TO_STRING_BUILDER_STYLE)
-                        .append(array[i], i).toString();
-                // System.out.println(i + "=" + valueString);
-            }
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-
- */
 
 }
 
-/*
-
-   call.enqueue(new Callback<ArrayList<KlineModel>>() {
-            @Override
-            public void onResponse(Call<ArrayList<KlineModel>> call, Response<ArrayList<KlineModel>> response) {
-                S
-
-
-            }
-
-            @Override
-            public void onFailure(Call<ArrayList<KlineModel>> call, Throwable t) {
-
-                t.printStackTrace();
-                System.out.println("+++++++   " + t);
-            }
-        });
-
-
-
-   Gson gson = new GsonBuilder()
-                .create();
-
-
-        Dispatcher dispatcher = new Dispatcher();
-        dispatcher.setMaxRequestsPerHost(500);
-        dispatcher.setMaxRequests(500);
-        sharedClient = new OkHttpClient.Builder()
-                .dispatcher(dispatcher)
-                .pingInterval(20, TimeUnit.SECONDS)
-                .build();
-
-
-        retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .client(sharedClient)
-                .build();
-
-
-        try {
-            loadData();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    private void loadData() throws JSONException {
-        KriptoApi kriptoApi = retrofit.create(KriptoApi.class);
 
 
 
 
-    }
 
- */
