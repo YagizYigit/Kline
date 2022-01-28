64cef848a779f7b27f90d367a4e8ff3080d2bf54
YagizYigit <71469815+YagizYigit@users.noreply.github.com>
2022-01-28 20:08:39 +0300

    Update KlineModel.java

--- src/main/java/com/serdaryigit/kline/KlineModel.java
+++ src/main/java/com/serdaryigit/kline/KlineModel.java
@@ -171,25 +171,7 @@ public class KlineModel {
         this.igNoreKline = igNoreKline;
     }
 
-    public void jasonParsee() {
-        String jsonString = "[[1642850280000,\"35291.18000000\",\"35318.00000000\",\"35250.15000000\",\"35304.72000000\",\"114.16537000\",1642850339999,\"4027913.53985850\",1368,\"61.48630000\",\"2168980.00679440\",\"0\"],[1642850340000,\"35304.73000000\",\"35305.26000000\",\"35265.89000000\",\"35280.60000000\",\"34.95279000\",1642850399999,\"1233376.84292890\",980,\"15.87426000\",\"560150.87425420\",\"0\"]]";
-        try {
-            JSONArray jsonArray = new JSONArray(jsonString);
-            JSONArray jsonArray2 = jsonArray.getJSONArray(0);
 
-            for (int i = 0; i < jsonArray2.length(); i++) {
-                String valueString = jsonArray2.getString(i);
-                Log.e("json", (" " + array[i] + " " + " : " + valueString));
-                new ToStringBuilder(this, BinanceApiConstants.TO_STRING_BUILDER_STYLE)
-                        .append(array[i], i).toString();
-
-            }
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return;
-    }
 
 
 }
